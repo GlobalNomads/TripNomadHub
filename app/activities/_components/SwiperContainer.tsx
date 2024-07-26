7980ca1c281b678be08b2cf2a28801e0b4cfe32f
jinyun <truejinyun@gmail.com>
2024-07-26 22:25:12 +0900

    feat: 불필요한 코드 제거

--- app/activities/_components/SwiperContainer.tsx
+++ app/activities/_components/SwiperContainer.tsx
@@ -2,7 +2,7 @@
     체험 상세 페이지의 mobile 버전 이미지 갤러리
 */
 
-import React, { useEffect, useState } from "react";
+import React from "react";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
@@ -15,31 +15,8 @@ interface SwiperContainerProps {
 }
 
 const SwiperContainer: React.FC<SwiperContainerProps> = ({ images }) => {
-  const [swiperInstance, setSwiperInstance] = useState<any>(null);
-
-  useEffect(() => {
-    if (swiperInstance) {
-      const handleProgress = (e: CustomEvent) => {
-        console.log(e.detail);
-      };
-
-      const handleSlideChange = () => {
-        console.log("slide changed");
-      };
-
-      swiperInstance.on("progress", handleProgress);
-      swiperInstance.on("slideChange", handleSlideChange);
-
-      return () => {
-        swiperInstance.off("progress", handleProgress);
-        swiperInstance.off("slideChange", handleSlideChange);
-      };
-    }
-  }, [swiperInstance]);
-
   return (
     <Swiper
-      onSwiper={swiper => setSwiperInstance(swiper)}
       modules={[Navigation, Pagination, Scrollbar]}
       spaceBetween={50}
       slidesPerView={1}
