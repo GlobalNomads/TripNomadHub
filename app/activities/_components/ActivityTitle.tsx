84a51975183cb9b192e9679b7d26aef78bb7788d
jinyun <truejinyun@gmail.com>
2024-07-26 14:02:37 +0900

    feat: 컨텐츠 작성

--- app/activities/_components/ActivityTitle.tsx
+++ app/activities/_components/ActivityTitle.tsx
@@ -1,5 +1,20 @@
+import React from 'react'
+
 function ActivityTitle() {
-    return <div>ActivityTitle</div>;
+    return (
+    <>
+        <div className="md-regular pb-[10px]">
+            카테고리
+        </div>
+        <div className="text-2xl-bold md:text-3xl-bold xl:text-3xl-bold pb-[16px]">
+            제목
+        </div>
+        <div className="md-regular">
+            <span>별점</span> 
+            <span>주소</span>
+        </div>
+    </>
+    );
   }
   
   export default ActivityTitle;
