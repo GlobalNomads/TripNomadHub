d9981066507cf8855d2d74f96bb6595f3e358b93
JiminN2 <162524947+JiminN2@users.noreply.github.com>
2024-07-27 01:57:55 +0900

    chore: 불필요한 코드 삭제

--- app/myreservations/ReservationCard.tsx
+++ app/myreservations/ReservationCard.tsx
@@ -1,8 +1,6 @@
-function ReservationCard():React.JSX.Element {
-    return(
-<div className="mx-auto border-solid bg-white border rounded-3xl border-gray-300 w-[344px] h-[128px]">
-    card
-</div>
-    )
-};
+function ReservationCard(): React.JSX.Element {
+  return (
+    <div className="mx-auto h-[128px] w-[344px] rounded-3xl border border-solid border-gray-300 bg-white">card</div>
+  );
+}
 export default ReservationCard;
