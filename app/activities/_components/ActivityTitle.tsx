63e41d8f2486a0d23187468b43a5f68627caaefe
jinyun <truejinyun@gmail.com>
2024-07-26 15:18:29 +0900

    feat: MockData 연결

--- app/activities/_components/ActivityTitle.tsx
+++ app/activities/_components/ActivityTitle.tsx
@@ -7,26 +7,34 @@
 import React from 'react';
 import Image from 'next/image';
 import star from '@/app/assets/icon/ic_star_on.svg';
-import location from '@/app/assets/icon/ic_location.svg';
+import locationIcon from '@/app/assets/icon/ic_location.svg';
 
-function ActivityTitle() {
+interface ActivityTitleProps {
+    category: string;
+    title: string;
+    rating: number;
+    reviewCount: number;
+    location: string;
+  }
+
+  const ActivityTitle: React.FC<ActivityTitleProps> = ({ category, title, rating, reviewCount, location }) => {
     return (
     <>
        <div className="md-regular pb-[10px]">
-            카테고리
+            {category}
         </div>
         <div className="text-2xl-bold md:text-3xl-bold xl:text-3xl-bold pb-[16px]">
-            제목
+            {title}
         </div> 
         <div className="md-regular flex items-center space-x-3">
             <span className="flex items-center space-x-[4px] md:space-x-[6px] xl:space-x-[6px]">
                 <Image src={star} alt="rating" width={16} height={16} />
-                <span>별점</span>
-                <span>(총리뷰수)</span>
+                <span>{rating}</span>
+                <span>({reviewCount})</span>
             </span>
         <span className="flex items-center space-x-[2px]">
-             <Image src={location} alt="location" width={18} height={18} />
-            <span>주소</span>
+            <Image src={locationIcon} alt="location" width={18} height={18} />
+            <span>{location}</span>
         </span>
       </div>
     </>
