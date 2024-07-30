52fd568891e51f8317997852136a4b83844cc713
jinyun <truejinyun@gmail.com>
2024-07-31 00:20:29 +0900

    feat: Image 컴포넌트 사이즈 지정

--- app/activities/_components/ActivityImageGallery.tsx
+++ app/activities/_components/ActivityImageGallery.tsx
@@ -29,11 +29,23 @@ const ActivityImageGallery: React.FC<ImageGalleryProps> = ({ images, bannerImage
       {/* 태블릿과 데스크탑 그리드 */}
       <div className="hidden h-[600px] grid-cols-4 grid-rows-2 gap-4 md:grid xl:grid">
         <div className="relative col-span-2 row-span-2">
-          <Image src={bannerImage} alt="Banner" layout="fill" className="object-cover" />
+          <Image
+            src={bannerImage}
+            alt="Banner"
+            fill
+            className="object-cover"
+            sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+          />
         </div>
         {images.slice(0, 4).map((image, index) => (
           <div key={index} className="relative h-full">
-            <Image src={image} alt={`Image ${index + 1}`} layout="fill" className="object-cover" />
+            <Image
+              src={image}
+              alt={`Image ${index + 1}`}
+              fill
+              className="object-cover"
+              sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+            />
           </div>
         ))}
       </div>
