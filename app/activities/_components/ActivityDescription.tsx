dc1d40fecaa316c5c37107e10432a76d8186144c
jinyun <truejinyun@gmail.com>
2024-07-31 14:11:28 +0900

    feat: 주석 추가 및 divider 스타일 수정

--- app/activities/_components/ActivityDescription.tsx
+++ app/activities/_components/ActivityDescription.tsx
@@ -1,5 +1,6 @@
 /*
     체험 상세 페이지 체험 설명 컴포넌트
+    TODO: API 연결(현재: MockData 연결됨)
 */
 
 import { FC } from "react";
@@ -10,12 +11,15 @@ interface ActivityDescriptionProps {
 
 const ActivityDescription: FC<ActivityDescriptionProps> = ({ description }) => {
   return (
-    <div className="with-full px-6 py-4 text-primary-black-100">
-      <hr className="my-4 border-t border-gray-300" />
-      <div className="pb-4 text-[20px] font-bold">체험설명</div>
-      <div className="text-lg-semibold">{description}</div>
-      <hr className="my-4 border-t border-gray-300" />
-    </div>
+    <>
+      <hr className="my-4 hidden border-t border-gray-300 md:block xl:block" />
+      <div className="with-full px-6 py-4 text-primary-black-100">
+        <div className="pb-4 text-[20px] font-bold">체험설명</div>
+        <div className="text-lg-semibold">{description}</div>
+        <hr className="my-4 border-t border-gray-300 md:hidden xl:hidden" />
+      </div>
+      <hr className="my-4 hidden border-t border-gray-300 md:block xl:block" />
+    </>
   );
 };
 
