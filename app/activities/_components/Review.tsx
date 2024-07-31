636e0007596c9e8d1a21e96159f1c3a2ca57d508
jinyun <truejinyun@gmail.com>
2024-07-31 20:33:21 +0900

    feat: Review컴포넌트 UI 완성

--- app/activities/_components/Review.tsx
+++ app/activities/_components/Review.tsx
@@ -2,18 +2,34 @@
     후기 컴포넌트
 */
 
+import Image from "next/image";
 import { FC } from "react";
 
 const Review: FC<{ review: any }> = ({ review }) => {
   return (
-    <>
-      <div>
-        <img src={review.user.profileImageUrl} alt={review.user.nickname} />
-        <div>{review.user.nickname}</div>
-        <div>{new Date(review.createdAt).toLocaleDateString()}</div>
-        <div>{review.content}</div>
+    <div className="flex rounded-lg border p-4">
+      <div className="mr-4 flex-shrink-0">
+        <div className="h-[45px] w-[45px] overflow-hidden rounded-full">
+          <Image
+            src={review.user.profileImageUrl}
+            width={45}
+            height={45}
+            alt={review.user.nickname}
+            className="object-cover"
+          />
+        </div>
       </div>
-    </>
+      <div className="flex-grow">
+        <div className="mb-2 flex items-baseline">
+          <div className="text-lg-semibold text-primary-black-100">{review.user.nickname}</div>
+          <div className="mx-2 text-primary-black-100">|</div>
+          <div className="whitespace-nowrap text-lg-regular text-primary-gray-600">
+            {new Date(review.createdAt).toLocaleDateString()}
+          </div>
+        </div>
+        <div className="mt-2 text-lg-regular text-primary-black-100">{review.content}</div>
+      </div>
+    </div>
   );
 };
 
