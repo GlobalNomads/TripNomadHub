547aa3b7ce61c3fb1acdea29ca58bc0eac7445e6
JiminN2 <162524947+JiminN2@users.noreply.github.com>
2024-07-31 14:24:01 +0900

    feat: 프로필 이미지 디폴트값 추가 및 api 연결

---
+++ app/myreservations/_components/PencilButton.tsx
@@ -0,0 +1,21 @@
+// import Image from "next/image";
+// import { useEffect, useState } from "react";
+
+// const PencilButton=() =>{
+//     const
+
+//     const token =
+//     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6Njk3LCJ0ZWFtSWQiOiI2LTExIiwiaWF0IjoxNzIyMTQ1ODkyLCJleHAiOjE3MjMzNTU0OTIsImlzcyI6InNwLWdsb2JhbG5vbWFkIn0.OpRMgEWUfrFDzmzr8nO0rbMlh1BlmQBpEpwkgcAFLVQ";
+
+//     useEffect(() => {
+//         const ChangeProfile = async() => {
+//             try {
+//                 const url = `${process.env.NEXT_PUBLIC_BASE_URL}users/me`;
+
+//             }
+//         }
+//     })
+
+// }
+
+// <Image className="absolute right-14 top-[140px] xl:right-32" src={Pencil} width={44} height={44} alt="연필" />
--- app/myreservations/_components/ProfileImage.tsx
+++ app/myreservations/_components/ProfileImage.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import { useEffect, useState } from "react";
 
 const ProfileImage = () => {
-  const [profileImageUrl, setProfileImageUrl] = useState("");
+  const [profileImageUrl, setProfileImageUrl] = useState<string | null>(null);
 
   const token =
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6Njk3LCJ0ZWFtSWQiOiI2LTExIiwiaWF0IjoxNzIyMTQ1ODkyLCJleHAiOjE3MjMzNTU0OTIsImlzcyI6InNwLWdsb2JhbG5vbWFkIn0.OpRMgEWUfrFDzmzr8nO0rbMlh1BlmQBpEpwkgcAFLVQ";
@@ -24,10 +24,9 @@ const ProfileImage = () => {
 
         const jsonData = await response.json();
         setProfileImageUrl(jsonData.profileImageUrl);
-
-        console.log("Profile Image URL:", jsonData.profileImageUrl);
       } catch (error) {
         console.error("Fetch profile failed:", error);
+        setProfileImageUrl(null);
       }
     };
 
@@ -41,11 +40,12 @@ const ProfileImage = () => {
           src={profileImageUrl}
           width={160}
           height={160}
+          priority
           alt="프로필 이미지"
           className="flex h-[160px] w-[160px] justify-center rounded-full object-cover"
         />
       ) : (
-        <p>Loading...</p>
+        <div className="flex h-[160px] w-[160px] justify-center rounded-full bg-gray-300" />
       )}
     </div>
   );
