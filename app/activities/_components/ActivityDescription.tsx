08fc83e8712d3dfe5b2b8358c3f3a5a423ef2869
jinyun <truejinyun@gmail.com>
2024-07-31 14:02:58 +0900

    feat: 구분선 및 주석 추가

--- app/activities/_components/ActivityDescription.tsx
+++ app/activities/_components/ActivityDescription.tsx
@@ -1,3 +1,7 @@
+/*
+    체험 상세 페이지 체험 설명 컴포넌트
+*/
+
 import { FC } from "react";
 
 interface ActivityDescriptionProps {
@@ -7,8 +11,10 @@ interface ActivityDescriptionProps {
 const ActivityDescription: FC<ActivityDescriptionProps> = ({ description }) => {
   return (
     <div className="with-full px-6 py-4 text-primary-black-100">
+      <hr className="my-4 border-t border-gray-300" />
       <div className="pb-4 text-[20px] font-bold">체험설명</div>
       <div className="text-lg-semibold">{description}</div>
+      <hr className="my-4 border-t border-gray-300" />
     </div>
   );
 };
