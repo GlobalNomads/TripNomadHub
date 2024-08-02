8b4d57b861f85e5aa5b91bb4171245939d5449ac
YoungUnKim <162089313+YoungUnKim@users.noreply.github.com>
2024-08-02 16:10:07 +0900

    feat: ?

--- app/components/Header/Header.tsx
+++ app/components/Header/Header.tsx
@@ -7,7 +7,7 @@ function Header() {
       <div className="flex w-full max-w-[1200px] justify-between">
         <Image src={HeaderLogo} alt="헤더 로고 이미지"></Image>
         <Image src={HeaderLogo} alt="헤더 로고 이미지"></Image> {/* 임시로 위치 배정을 위해 아이콘을 넣어둠 */}
-        {/* <HeaderForm /> 위 이미지 대신 이 부분이 반영될 예정*/}
+        {/* <HeaderForm /> 위 이미지 대신 이 부분이 반영될 예정 a*/}
       </div>
     </header>
   );
