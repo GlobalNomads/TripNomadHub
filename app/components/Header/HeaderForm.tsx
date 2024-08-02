b9066a3be78b33a4d738e2b5191dcb9479abfb99
YoungUnKim <162089313+YoungUnKim@users.noreply.github.com>
2024-08-02 22:53:08 +0900

    feat: 스타일 수정

--- app/components/Header/HeaderForm.tsx
+++ app/components/Header/HeaderForm.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 function HeaderForm() {
   return (
     <div className="flex items-center gap-[25px]">
-      <Link href="/signin" className="font-text-md-medium text-primary-black-200">
+      <Link href="/signin" className="text-md-medium text-primary-black-200">
         로그인
       </Link>
       <Link href="/signup" className="text-md-medium text-primary-black-200">
