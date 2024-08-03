65187c9fd76ee20446a58ea93a63f682aaf6559c
김세민 <ksm6430@naver.com>
2024-08-03 18:22:17 +0900

    MainPage UI 작업 입니다

---
+++ app/components/MainPage/Activity.tsx
@@ -0,0 +1,37 @@
+import Image from "next/image";
+import Link from "next/link";
+
+import miniStar from "@/assets/icon/mini_star.svg";
+
+import type { Activites } from "./types";
+
+const Activity = ({ data }: { data: Activites }) => {
+  const { id, title, price, rating, reviewCount, bannerImageUrl } = data;
+
+  return (
+    <Link href={`/activity/${id}`} className="group w-full">
+      <div className="relative aspect-square w-full">
+        <Image
+          src={bannerImageUrl}
+          fill
+          alt="banner"
+          className="rounded-2xl object-cover shadow-none transition-all duration-300 group-hover:translate-y-[-3px] group-hover:shadow-lg group-hover:shadow-primary-gray-300"
+        />
+      </div>
+
+      <div className="my-4 flex items-center gap-5 text-md-semibold md:my-8 md:text-lg-semibold">
+        <Image src={miniStar} alt="Star" width={16} height={16} />
+        <span>
+          {rating} <span className="text-primary-gray-450">({reviewCount})</span>
+        </span>
+      </div>
+      <h1 className="line-clamp-2 text-2lg-semibold text-primary-black-200 md:text-2xl-semibold">{title}</h1>
+      <div className="mb-4 mt-12">
+        <span className="text-2xl-bold">₩ {price.toLocaleString()} </span>
+        <span className="text-primary-gray-450">/ 인</span>
+      </div>
+    </Link>
+  );
+};
+
+export default Activity;
---
+++ app/components/MainPage/CategoryFilter.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import React from "react";
+import CategoryButton from "@/components/Button/CategoryButton";
+
+const CATEGORIES = ["문화·예술", "식음료", "스포츠", "투어", "관광", "웰빙"];
+
+const CategoryFilter = () => {
+  return (
+    <div className="relative">
+      <div className="no-scrollbar flex gap-4 overflow-x-auto md:gap-8 xl:gap-12">
+        {CATEGORIES.map(each => (
+          <CategoryButton
+            key={each}
+            className="flex-shrink-0 border border-primary-black-100 text-primary-black-100"
+          >
+            {each}
+          </CategoryButton>
+        ))}
+        <div className="ml-16"></div>
+      </div>
+      <div className="pointer-events-none absolute right-0 top-0 h-full w-[50px] bg-gradient-to-l from-white to-transparent" />
+    </div>
+  );
+};
+
+export default CategoryFilter;
