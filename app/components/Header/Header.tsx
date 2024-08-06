0e0a961283e258e6ca67dd90bae2bf6dfad5cc2c
jinyun <truejinyun@gmail.com>
2024-08-06 15:58:01 +0900

    feat: logo에 priority 속성 추가

--- app/components/Header/Header.tsx
+++ app/components/Header/Header.tsx
@@ -8,7 +8,7 @@ function Header() {
     <header className="sticky top-0 z-10 flex justify-center gap-x-[40px] border border-b-[1px] border-primary-gray-300 bg-white p-[20px]">
       <div className="flex w-full max-w-[1200px] justify-between">
         <Link href="/">
-          <Image src={HeaderLogo} alt="헤더 로고 이미지" />
+          <Image src={HeaderLogo} alt="헤더 로고 이미지" priority />
         </Link>
         <HeaderForm />
       </div>
