b645fb3b8283d08b79acaebc9d1125e227761fad
sermain2 <87350415+sermain2@users.noreply.github.com>
2024-08-07 20:13:31 +0900

    feat: MainPage UI (#30)
    * fix: MainPage UI 수정

--- app/components/Footer/Footer.tsx
+++ app/components/Footer/Footer.tsx
@@ -8,10 +8,10 @@ import YoutubeIcon from "@icon/YoutubeIcon";
 
 const Footer = () => {
   return (
-    <footer className="bg-primary-black-100 py-8">
+    <footer className="mt-32 bg-primary-black-100 py-8">
       <div className="container mx-auto flex flex-col items-center px-8 md:flex-row md:justify-between md:px-16 lg:px-32">
-        <div className="mb-4 text-sm text-gray-600 md:mb-0">&copy;codeit - 2023</div>
-        <div className="mb-4 flex gap-8 md:mb-0">
+        <div className="text-sm text-gray-600 md:mb-0">&copy;codeit - 2023</div>
+        <div className="flex gap-8 md:mb-0">
           <Link href="/privacypolicy" className="text-sm text-gray-400">
             Privacy Policy
           </Link>
