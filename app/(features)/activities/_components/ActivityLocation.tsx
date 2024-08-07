cffca628ee1b2a8d077790b9db3b2d450fd65455
jinyun <truejinyun@gmail.com>
2024-08-07 15:22:08 +0900

    feat: Image컴포넌트에 height 속성 추가

--- app/(features)/activities/_components/ActivityLocation.tsx
+++ app/(features)/activities/_components/ActivityLocation.tsx
@@ -70,7 +70,7 @@ const ActivityLocation: React.FC<ActivityLocationProps> = ({ address }) => {
       <h3 className="text-xl-bold">미팅 포인트</h3>
       <div id="map" className="map-container"></div>
       <div className="flex space-x-2">
-        <Image src={LocationMarker} width={20} alt="location" />
+        <Image src={LocationMarker} width={20} height={20} alt="location" />
         <p className="text-md-semibold">{address}</p>
       </div>
     </div>
