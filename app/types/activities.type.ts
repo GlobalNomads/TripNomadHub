83ad47340f86cde0b20647de5dc506b045812acc
jinyun <truejinyun@gmail.com>
2024-08-08 16:11:26 +0900

    feat: activity 타입 정의 수정

--- app/types/activities.type.ts
+++ app/types/activities.type.ts
@@ -29,7 +29,7 @@ export interface ReviewUser {
   id: number;
 }
 
-export interface ReviewList {
+export interface Review {
   id: number;
   user: ReviewUser;
   activityId: number;
@@ -39,55 +39,94 @@ export interface ReviewList {
   updatedAt: string;
 }
 
-export interface ActivitiesReviewData {
+export interface Reviews {
   averageRating: number;
   totalCount: number;
-  reviews: ReviewList[];
-}
-
-export interface ScheduleTimeList {
-  endTime: string;
-  startTime: string;
-  id: number;
+  reviews: Review[];
 }
 
 export interface ScheduleData {
   date: string;
-  times: ScheduleTimeList[];
+  times: Schedule[];
 }
 
-export interface SubImageUrls {
-  id?: number;
-  imageUrl: string;
-}
-
-export interface Schedules {
-  id?: number;
-  date: string;
-  startTime: string;
-  endTime: string;
-}
-
-export interface ActivitiesDataProp {
-  id?: number;
-  userId?: number;
+export interface Activity {
+  id: number;
+  userId: number;
   title: string;
   description: string;
   category: string;
   price: number;
   address: string;
   bannerImageUrl: string;
-  subImageUrls: SubImageUrls[];
-  reviewCount?: number;
-  rating?: number;
-  createdAt?: string;
-  updatedAt?: string;
-}
-
-export interface ActivitiesIdData extends ActivitiesDataProp {
-  schedules: Schedules[];
+  rating: number;
+  reviewCount: number;
+  createdAt: string;
+  updatedAt: string;
+  subImages: { id: number; imageUrl: string }[];
+  schedules: { id: number; date: string; startTime: string; endTime: string }[];
 }
 
 export interface PostActivities {
   schedules: ScheduleData[];
+}
+
+export interface ActivityPageProps {
+  params: { activityId: string };
+}
+
+export interface ActivityTitleProps {
+  category: string;
+  title: string;
+  rating: number;
+  reviewCount: number;
+  location: string;
+}
+
+export interface ParticipantCountProps {
+  count: number;
+  setCount: (count: number) => void;
+}
+
+export interface PriceInfoProps {
+  price: number;
+}
+
+export interface Schedule {
+  id: number;
+  date: string;
+  startTime: string;
+  endTime: string;
+}
+
+export interface ReservationFloatingBoxProps {
+  schedules: Schedule[];
+  price: number;
+}
+
+export interface ScheduleSelectorProps {
+  schedules: Schedule[];
+  setSelectedSchedule: (schedule: string) => void;
+}
+
+export interface TotalPriceProps {
+  price: number;
+  count: number;
+}
+
+export interface ActivityDescriptionProps {
+  description: string;
+}
+
+export interface ImageGalleryProps {
+  images: string[];
+  bannerImage: string;
+}
+
+export interface ActivityLocationProps {
+  address: string;
+}
+
+export interface SwiperContainerProps {
+  images: string[];
 }
