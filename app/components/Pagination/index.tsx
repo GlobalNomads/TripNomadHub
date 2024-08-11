6b8c06860f27004277a0410de0b154f56385a234
jinyun <truejinyun@gmail.com>
2024-08-11 15:05:59 +0900

    feat: 페이지네이션 코드 수정

--- app/components/Pagination/index.tsx
+++ app/components/Pagination/index.tsx
@@ -11,11 +11,9 @@ interface PaginationProps {
 }
 
 const Pagination: React.FC<PaginationProps> = ({ totalPages, currentPage, onPageChange }) => {
-  // 페이지 변경을 처리하는 함수
   const handlePageChange = (page: number) => {
     if (page > 0 && page <= totalPages) {
-      // 페이지가 1 이상이고 총 페이지 수 이내인지 확인
-      onPageChange(page); // 유효한 경우 페이지 변경 함수 호출
+      onPageChange(page);
     }
   };
 
@@ -26,30 +24,24 @@ const Pagination: React.FC<PaginationProps> = ({ totalPages, currentPage, onPage
         disabled={currentPage === 1}
         type={currentPage === 1 ? "disabled" : "white"}
       >
-        <Image src={PaginationArrowLeft} alt="Previous Page" width={24} height={24} /> {/* 왼쪽 화살표 아이콘 */}
+        <Image src={PaginationArrowLeft} alt="Previous Page" width={24} height={24} />
       </Button.Pagination>
-      {Array.from(
-        { length: totalPages },
-        (
-          _,
-          index, // 총 페이지 수만큼 배열 생성 후 각 페이지 버튼 생성
-        ) => (
-          <Button.Pagination
-            key={index + 1}
-            onClick={() => handlePageChange(index + 1)}
-            isActive={currentPage === index + 1}
-            type={currentPage === index + 1 ? "nomadBlack" : "white"}
-          >
-            {index + 1}
-          </Button.Pagination>
-        ),
-      )}
+      {Array.from({ length: totalPages }, (_, index) => (
+        <Button.Pagination
+          key={index + 1}
+          onClick={() => handlePageChange(index + 1)}
+          isActive={currentPage === index + 1}
+          type={currentPage === index + 1 ? "nomadBlack" : "white"}
+        >
+          {index + 1}
+        </Button.Pagination>
+      ))}
       <Button.Pagination
         onClick={() => handlePageChange(currentPage + 1)}
         disabled={currentPage === totalPages}
         type={currentPage === totalPages ? "disabled" : "white"}
       >
-        <Image src={PaginationArrowRight} alt="Next Page" width={24} height={24} /> {/* 오른쪽 화살표 아이콘 */}
+        <Image src={PaginationArrowRight} alt="Next Page" width={24} height={24} />
       </Button.Pagination>
     </div>
   );
