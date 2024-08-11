6a74f070db5823e522fc401619570eb6d15e0dc7
JiminN2 <162524947+JiminN2@users.noreply.github.com>
2024-08-11 16:39:45 +0900

    feat: 프로필이미지 useQuery 로 변경

--- app/components/SideNav/ProfileImage.tsx
+++ app/components/SideNav/ProfileImage.tsx
@@ -1,39 +1,3 @@
-// "use client";
-// import getUsersMe from "@/api/Users/getUsersMe";
-// import { UserData } from "@/types/users.type";
-// import Image from "next/image";
-// import { useEffect, useState } from "react";
-
-// const ProfileImage = () => {
-//   const [userData, setUserData] = useState<UserData | null>(null);
-
-//   useEffect(() => {
-//     const fetchData = async () => {
-//       try {
-//         const data = await getUsersMe();
-//         setUserData(data);
-//       } catch (error) {
-//         console.error("Failed to fetch user data:", error);
-//       }
-//     };
-//     fetchData();
-//   }, []);
-
-//   if (!userData) return <div>Loading...</div>;
-
-//   return (
-//     <div className="relative h-[160px] w-[160px] overflow-hidden rounded-full">
-//       {userData.profileImageUrl ? (
-//         <Image src={userData.profileImageUrl} priority alt="Profile picture" layout="fill" objectFit="cover" />
-//       ) : (
-//         <div className="flex h-full w-full items-center justify-center bg-primary-gray-300"></div>
-//       )}
-//     </div>
-//   );
-// };
-
-// export default ProfileImage;
-
 "use client";
 
 import getUsersMe from "@/api/Users/getUsersMe";
@@ -48,7 +12,7 @@ const ProfileImage = () => {
   } = useQuery({
     queryKey: ["getUsersMe"],
     queryFn: () => getUsersMe(),
-    // Only execute the query on the client-side
+
     enabled: typeof window !== "undefined",
   });
 
