529cbd1a93c9b6ac396ee46ff2b46baf4e4cd9ac
jinyun <truejinyun@gmail.com>
2024-08-13 01:49:07 +0900

    feat: 취소 모달 수정

--- app/components/Modal/CancelModal.tsx
+++ app/components/Modal/CancelModal.tsx
@@ -7,35 +7,32 @@ import Button from "@button/Button";
 import check from "@icon/ic_check_inCircle.svg";
 import Image from "next/image";
 import { FC } from "react";
-import DefaultModal, { ModalFunctionProps } from "./DefaultModal";
+import DefaultModal, { ModalBody, ModalFooter } from "./DefaultModal";
 
-interface CancelModalProps extends ModalFunctionProps {
+interface CancelModalProps {
+  isOpen: boolean;
+  onClose: () => void;
   onCancel: () => void;
   description: string;
 }
 
 const CancelModal: FC<CancelModalProps> = ({ isOpen, onClose, onCancel, description }) => {
   return (
-    <DefaultModal
-      isOpen={isOpen}
-      onClose={onClose}
-      width="w-[298px]"
-      height="h-[184px]"
-      footer={
-        <div className="mt-4 flex w-full justify-center space-x-2">
-          <Button.Default type="white" onClick={onClose} className="h-[38px] w-[80px] p-[7px]">
-            아니오
-          </Button.Default>
-          <Button.Default type="nomadBlack" onClick={onCancel} className="h-[38px] w-[80px] p-[7px]">
-            취소하기
-          </Button.Default>
+    <DefaultModal isOpen={isOpen} onClose={onClose} className="h-[184px] w-[300px]">
+      <ModalBody>
+        <div className="flex flex-col items-center">
+          <Image src={check} width={24} alt="확인메시지" className="mb-4" />
+          <p className="mb-4 text-center text-2lg-medium text-primary-black-100">{description}</p>
         </div>
-      }
-    >
-      <div className="flex flex-col items-center">
-        <Image src={check} width={24} alt="확인" className="mb-4" />
-        <p className="mb-4 text-center text-2lg-medium text-primary-black-100">{description}</p>
-      </div>
+      </ModalBody>
+      <ModalFooter className="flex justify-center space-x-2">
+        <Button.Default type="white" onClick={onClose} className="h-[38px] w-[80px] p-[7px]">
+          아니오
+        </Button.Default>
+        <Button.Default type="nomadBlack" onClick={onCancel} className="h-[38px] w-[80px] p-[7px]">
+          취소하기
+        </Button.Default>
+      </ModalFooter>
     </DefaultModal>
   );
 };
