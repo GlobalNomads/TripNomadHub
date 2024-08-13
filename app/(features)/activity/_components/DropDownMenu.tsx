7d649711d0bfe3f1f93361bb4ba91ed3f0aad0ed
jinyun <truejinyun@gmail.com>
2024-08-14 03:09:10 +0900

    feat: DropDown 수정

--- app/(features)/activity/_components/DropDownMenu.tsx
+++ app/(features)/activity/_components/DropDownMenu.tsx
@@ -14,29 +14,41 @@ const DropDownMenu: FC = () => {
 
   const handleEdit = () => {
     console.log("Edit action triggered");
+    setIsOpen(false);
   };
 
   const handleDelete = () => {
     console.log("Delete action triggered");
+    setIsOpen(false);
   };
 
   return (
-    <Dropdown
-      isOpen={isOpen}
-      onClose={() => setIsOpen(false)}
-      trigger={
-        <DropdownTrigger onClick={() => setIsOpen(!isOpen)}>
-          <Image src={KebabBtn} alt="menu" width={32} height={32} />
-        </DropdownTrigger>
-      }
-    >
-      <DropdownItem onClick={handleEdit} className="w-[140px] p-4 md:w-[160px]">
-        수정하기
-      </DropdownItem>
-      <DropdownItem onClick={handleDelete} className="w-[140px] p-4 md:w-[160px]">
-        삭제하기
-      </DropdownItem>
-    </Dropdown>
+    <div className="relative inline-block text-left">
+      <Dropdown
+        isOpen={isOpen}
+        onClose={() => setIsOpen(false)}
+        trigger={
+          <DropdownTrigger onClick={() => setIsOpen(!isOpen)}>
+            <Image src={KebabBtn} alt="menu" width={32} height={32} />
+          </DropdownTrigger>
+        }
+      >
+        <div className="absolute right-0 mt-2 w-[140px] origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none md:w-[160px]">
+          <DropdownItem
+            onClick={handleEdit}
+            className="block w-full items-center px-4 py-4 text-sm text-gray-700 hover:bg-gray-100"
+          >
+            수정하기
+          </DropdownItem>
+          <DropdownItem
+            onClick={handleDelete}
+            className="block w-full px-4 py-4 text-sm text-gray-700 hover:bg-gray-100"
+          >
+            삭제하기
+          </DropdownItem>
+        </div>
+      </Dropdown>
+    </div>
   );
 };
 
