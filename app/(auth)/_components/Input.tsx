8233f179b281f0307e4283c7876ab46b8f5d7b27
YoungUnKim <162089313+YoungUnKim@users.noreply.github.com>
2024-08-14 15:02:09 +0900

    feat: 비밀번호 감추기 보기 기능 추가

--- app/(auth)/_components/Input.tsx
+++ app/(auth)/_components/Input.tsx
@@ -1,23 +1,42 @@
-import React, { forwardRef } from "react";
-
+import useBoolean from "@/hooks/useBoolean";
+import UnLockIcon from "@icon/ic_btn_visibility_off.svg";
+import LockIcon from "@icon/ic_btn_visibility_on.svg";
+import Image from "next/image";
+import React, { forwardRef, useState } from "react";
 interface IInputProps extends React.InputHTMLAttributes<HTMLInputElement> {
   validationCheck?: boolean;
 }
 
 export default forwardRef(function Input(
-  { validationCheck, ...rest }: IInputProps,
+  { validationCheck, type, ...rest }: IInputProps,
   ref: React.LegacyRef<HTMLInputElement>,
 ) {
+  const [inputType, setInputType] = useState(type);
+  const { value: password, handleToggle: togglePassword } = useBoolean();
+
+  const handleIconClick = () => {
+    togglePassword();
+    setInputType(password ? "password" : "text");
+  };
+
   return (
-    <input
-      autoComplete="off"
-      className={`box-border w-full rounded-md border border-primary-gray-700 px-4 py-3 text-sm leading-[1.7] text-primary-gray-600 outline-0 placeholder:text-[#8F95B2] ${
-        validationCheck
-          ? "bg-primary-red-100 focus:border-primary-red-100 focus:bg-primary-red-100"
-          : "bg-primary-gray-100 focus:border-primary-green-200 focus:bg-white"
-      }`}
-      ref={ref}
-      {...rest}
-    />
+    <div className="relative">
+      <input
+        type={inputType}
+        autoComplete="off"
+        className={`box-border w-full rounded-md border border-primary-gray-400 px-4 py-3 text-sm-medium text-primary-gray-600 outline-0 placeholder:text-[#8F95B2] ${
+          validationCheck
+            ? "bg-primary-red-100 focus:border-primary-red-100 focus:bg-primary-red-100"
+            : "bg-primary-gray-100 focus:border-primary-green-200 focus:bg-white"
+        }`}
+        ref={ref}
+        {...rest}
+      />
+      {type === "password" && (
+        <span className="absolute right-[3%] top-[25%] max-w-[24px]" onClick={handleIconClick}>
+          {password ? <Image src={UnLockIcon} alt="비밀번호 보기" /> : <Image src={LockIcon} alt="비밀번호 감추기" />}
+        </span>
+      )}
+    </div>
   );
 });
