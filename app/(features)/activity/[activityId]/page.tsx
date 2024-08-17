96733ac61eded1e5adf33e2afd3ebc77a5a82b65
jinyun <truejinyun@gmail.com>
2024-08-17 13:51:38 +0900

    feat: 체험 생성자만 kebab button 보이도록 코드 추가

--- app/(features)/activity/[activityId]/page.tsx
+++ app/(features)/activity/[activityId]/page.tsx
@@ -7,17 +7,27 @@
     (4)예약완료시 예약완료 모달창 연결
 */
 
-import getActivitiesId from "@api/Activities/getActivitiesId"; // API 함수
+import getActivitiesId from "@api/Activities/getActivitiesId"; //체험 정보를 가져오는 API 함수
+import getUserData from "@api/Users/getUsersMe"; //로그인된 사용자 정보를 가져오는 API 함수
 import ActivityDescription from "../_components/ActivityDescription";
 import ActivityImageGallery from "../_components/ActivityImageGallery";
 import ActivityLocationServer from "../_components/ActivityLocationServer";
-import ActivityReview from "../_components/ActivityReview"; //
+import ActivityReview from "../_components/ActivityReview";
 import ActivityTitle from "../_components/ActivityTitle";
 import DropDownMenu from "../_components/DropDownMenu";
 import ReservationFloatingBox from "../_components/ReservationFloatingBox";
 
 export default async function ActivityPage({ params }: { params: { activityId: string } }) {
   const activity = await getActivitiesId(Number(params.activityId));
+  let user = null;
+
+  try {
+    user = await getUserData(); // 로그인한 사용자의 정보 가져오기
+  } catch (error) {
+    // 로그인하지 않은 경우 또는 사용자 정보를 가져오지 못한 경우, user는 null로 유지됩니다.
+  }
+
+  const isOwner = user && activity.userId === user.id; // 사용자가 만든 체험인지 확인
   const images = activity.subImages?.map(image => image.imageUrl) ?? [];
 
   return (
@@ -32,9 +42,7 @@ export default async function ActivityPage({ params }: { params: { activityId: s
             address={activity.address}
           />
         </div>
-        <div className="flex-none">
-          <DropDownMenu activityId={Number(params.activityId)} />
-        </div>
+        <div className="flex-none">{isOwner && <DropDownMenu activityId={Number(params.activityId)} />}</div>
       </div>
       <ActivityImageGallery bannerImage={activity.bannerImageUrl} images={images} />
       <div className="flex flex-col gap-6 md:flex-row md:gap-6">
