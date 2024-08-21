9008259050715a0781cd7507b707636bcc5b5b3d
jinyun <truejinyun@gmail.com>
2024-08-21 15:51:43 +0900

    feat: 내 체험 관리 페이지 추가

---
+++ app/(features)/(user)/myactivities/page.tsx
@@ -0,0 +1,26 @@
+// myActivities.tsx
+
+import DefaultButton from "@button/DefaultButton";
+import Link from "next/link";
+import React from "react";
+
+const MyActivities: React.FC = () => {
+  return (
+    <div>
+      <div className="mb-4 flex w-full items-center justify-between">
+        <h1 className="font-pretendard text-[32px] font-bold leading-[42px]">내 체험 관리</h1>
+        <Link href="/activity-registration">
+          <DefaultButton
+            type="nomadBlack"
+            className="flex h-[48px] w-[120px] items-center justify-center whitespace-nowrap p-[8px] text-sm md:p-[12px] md:text-base lg:p-[16px] lg:text-lg"
+            //          onClick={handleSubmit}
+          >
+            체험 등록하기
+          </DefaultButton>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default MyActivities;
