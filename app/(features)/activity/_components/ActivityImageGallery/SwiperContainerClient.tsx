68f6636c61d7a27525f7657ac68bd3f38bef5d0a
jinyun <truejinyun@gmail.com>
2024-08-22 04:03:05 +0900

    feat: 체험 상세 페이지 skeleton 적용

--- app/(features)/activity/_components/ActivityImageGallery/SwiperContainerClient.tsx
+++ app/(features)/activity/_components/ActivityImageGallery/SwiperContainerClient.tsx
@@ -1,10 +1,14 @@
 "use client";
 
+import SkeletonBox from "@skeleton/SkeletonBox";
 import dynamic from "next/dynamic";
 import React from "react";
 
 // SwiperContainer를 동적으로 가져오기
-const DynamicSwiperContainer = dynamic(() => import("./SwiperContainer"), { ssr: false });
+const DynamicSwiperContainer = dynamic(() => import("./SwiperContainer"), {
+  ssr: false,
+  loading: () => <SkeletonBox width="100%" height="300px" />, // 로딩 중 Skeleton 표시
+});
 
 interface SwiperContainerClientProps {
   images: string[];
--- app/(features)/activity/_components/ActivityImageGallery/index.tsx
+++ app/(features)/activity/_components/ActivityImageGallery/index.tsx
@@ -1,11 +1,12 @@
 /*
- *체험상세(activity) 페이지의 갤러리(ActivityImageGallery) 컴포넌트
- *체험상세 page에서 받아온 정보 중 bannerImage와 subImages의 url을 전달받아 display
- *성능 최적화를 위해 동적 기능(swiper)이 필요한 모바일 버전의 갤러리를 client component로분리함: SwiperContainerClient
+ * 체험상세(activity) 페이지의 갤러리(ActivityImageGallery) 컴포넌트
+ * 체험상세 page에서 받아온 정보 중 bannerImage와 subImages의 url을 전달받아 display
+ * 성능 최적화를 위해 동적 기능(swiper)이 필요한 모바일 버전의 갤러리를 client component로분리: SwiperContainerClient
+ * SkeletonScreen을 적용하면서 Image 컴포넌트를 ImageWithSkeleton 컴포넌트로 전환
  */
 
 import { ImageGalleryProps } from "@/types/activities.type";
-import Image from "next/image";
+import ImageWithSkeleton from "@skeleton/ImageWithSkeleton";
 import React from "react";
 import SwiperContainerClient from "./SwiperContainerClient";
 
@@ -15,37 +16,17 @@ const ActivityImageGallery: React.FC<ImageGalleryProps> = ({ images, bannerImage
       case 0:
         return (
           <div className="relative h-full w-full">
-            <Image
-              src={bannerImage}
-              alt="Banner"
-              fill
-              priority
-              className="object-cover"
-              sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-            />
+            <ImageWithSkeleton imageUrl={bannerImage} alt="체험 Banner 사진" priority />
           </div>
         );
       case 1:
         return (
           <div className="grid grid-cols-2 gap-1">
             <div className="relative" style={{ aspectRatio: "1 / 1" }}>
-              <Image
-                src={bannerImage}
-                alt="Banner"
-                fill
-                priority
-                className="object-cover"
-                sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-              />
+              <ImageWithSkeleton imageUrl={bannerImage} alt="체험 Banner 사진" priority />
             </div>
             <div className="relative" style={{ aspectRatio: "1 / 1" }}>
-              <Image
-                src={images[0]}
-                alt="SubImage 1"
-                fill
-                className="object-cover"
-                sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-              />
+              <ImageWithSkeleton imageUrl={images[0]} alt="체험 소개 사진 1" />
             </div>
           </div>
         );
@@ -53,32 +34,13 @@ const ActivityImageGallery: React.FC<ImageGalleryProps> = ({ images, bannerImage
         return (
           <div className="grid grid-cols-3 grid-rows-2 gap-1">
             <div className="relative col-span-2 row-span-2">
-              <Image
-                src={bannerImage}
-                alt="Banner"
-                fill
-                priority
-                className="object-cover"
-                sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-              />
+              <ImageWithSkeleton imageUrl={bannerImage} alt="체험 Banner 사진" priority />
             </div>
             <div className="relative col-span-1 row-span-1">
-              <Image
-                src={images[0]}
-                alt="SubImage 1"
-                fill
-                className="object-cover"
-                sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-              />
+              <ImageWithSkeleton imageUrl={images[0]} alt="체험 소개 사진 1" />
             </div>
             <div className="relative col-span-1 row-span-1">
-              <Image
-                src={images[1]}
-                alt="SubImage 2"
-                fill
-                className="object-cover"
-                sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-              />
+              <ImageWithSkeleton imageUrl={images[1]} alt="체험 소개 사진 2" />
             </div>
           </div>
         );
@@ -86,41 +48,16 @@ const ActivityImageGallery: React.FC<ImageGalleryProps> = ({ images, bannerImage
         return (
           <div className="grid grid-cols-3 grid-rows-3 gap-1">
             <div className="relative col-span-2 row-span-3">
-              <Image
-                src={bannerImage}
-                alt="Banner"
-                fill
-                priority
-                className="object-cover"
-                sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-              />
+              <ImageWithSkeleton imageUrl={bannerImage} alt="체험 Banner 사진" priority />
             </div>
             <div className="relative col-span-1 row-span-1">
-              <Image
-                src={images[0]}
-                alt="SubImage 1"
-                fill
-                className="object-cover"
-                sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-              />
+              <ImageWithSkeleton imageUrl={images[0]} alt="체험 소개 사진 1" />
             </div>
             <div className="relative col-span-1 row-span-1">
-              <Image
-                src={images[1]}
-                alt="SubImage 2"
-                fill
-                className="object-cover"
-                sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-              />
+              <ImageWithSkeleton imageUrl={images[1]} alt="체험 소개 사진 2" />
             </div>
             <div className="relative col-span-1 row-span-1">
-              <Image
-                src={images[2]}
-                alt="SubImage 3"
-                fill
-                className="object-cover"
-                sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-              />
+              <ImageWithSkeleton imageUrl={images[2]} alt="체험 소개 사진 3" />
             </div>
           </div>
         );
@@ -128,24 +65,11 @@ const ActivityImageGallery: React.FC<ImageGalleryProps> = ({ images, bannerImage
         return (
           <div className="grid grid-cols-4 grid-rows-2 gap-1">
             <div className="relative col-span-2 row-span-2">
-              <Image
-                src={bannerImage}
-                alt="Banner"
-                fill
-                priority
-                className="object-cover"
-                sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-              />
+              <ImageWithSkeleton imageUrl={bannerImage} alt="체험 Banner 사진" priority />
             </div>
             {images.slice(0, 4).map((image, index) => (
               <div key={index} className="relative col-span-1 row-span-1">
-                <Image
-                  src={image}
-                  alt={`SubImage ${index + 1}`}
-                  fill
-                  className="object-cover"
-                  sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-                />
+                <ImageWithSkeleton imageUrl={image} alt={`체험 소개 사진 ${index + 1}`} />
               </div>
             ))}
           </div>
