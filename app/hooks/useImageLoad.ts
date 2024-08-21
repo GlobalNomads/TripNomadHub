317e3e9cc283e1c51b0be26fec5ca8ccdcedb23d
jinyun <truejinyun@gmail.com>
2024-08-21 13:31:27 +0900

    feat: useImageLoad에 loading 추가

--- app/hooks/useImageLoad.ts
+++ app/hooks/useImageLoad.ts
@@ -2,26 +2,32 @@ import { useEffect, useState } from "react";
 
 const useImageLoad = (imageUrl?: string | null) => {
   const [imgError, setImgError] = useState<boolean | undefined>();
+  const [loading, setLoading] = useState<boolean>(true);
 
   useEffect(() => {
     if (!imageUrl) {
       setImgError(true);
+      setLoading(false);
       return;
     }
 
     const img = new Image();
     img.src = imageUrl;
 
+    setLoading(true); // 이미지 로드 시작
+
     img.onload = () => {
       setImgError(false);
+      setLoading(false); // 로드 성공
     };
 
     img.onerror = () => {
       setImgError(true);
+      setLoading(false); // 로드 실패
     };
   }, [imageUrl]);
 
-  return imgError;
+  return { imgError, loading };
 };
 
 export default useImageLoad;
