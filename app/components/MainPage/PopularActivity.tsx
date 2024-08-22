602ee0dc7bd50e608d4070c678160161e17e76ba
sermain2 <87350415+sermain2@users.noreply.github.com>
2024-08-22 13:15:00 +0900

    feat: 메인페이지 카테고리 필터 기능 (#62)
    * Revert "feat: MainPage API 작업"
    
    This reverts commit b595f1a87b2f2220997498495e7a94c390fea80e.
    
    * feat: category 기능 추가
    
    * feat: 배너 기능 추가
    
    * refactor: remove .env file
    
    * refactor: @icon/ic_default_reviewprofile.png 변경
    
    * refactor: env 삭제
    
    * refactor: 드롭 박스 & 메뉴 사이즈 조절, 배너 ui 조절
    
    * feat: Category Css 추가

--- app/components/MainPage/PopularActivity.tsx
+++ app/components/MainPage/PopularActivity.tsx
@@ -18,27 +18,28 @@ const PopularActivity = ({ data }: { data: ActivityList }) => {
   return (
     <Link
       href={`/activity/${id}`}
-      className="relative size-[186px] flex-shrink-0 rounded-[20px] px-[20px] pb-[24px] text-white shadow-none transition-all duration-300 hover:translate-y-[-3px] hover:shadow-lg hover:shadow-primary-gray-300 md:size-[384px]"
+      className="relative size-[186px] flex-shrink-0 rounded-[20px] px-[20px] pb-[24px] text-white shadow-none transition-all duration-300 hover:translate-y-[-3px] hover:shadow-lg hover:shadow-gray-400 md:w-[384px] md:h-[384px]"
     >
       <Image
         src={displayImage || EmptyImage}
         alt="banner"
         fill
-        className="absolute -z-10 rounded-[20px] brightness-75 filter"
+        sizes="(max-width: 768px) 50vw, (max-width: 1200px) 33vw, 25vw"
+        className="absolute -z-10 rounded-[20px] object-cover brightness-75 filter"
       />
 
-      <div className="mt-[48px] flex items-center gap-5 text-md-semibold leading-[24px] md:mt-[174px]">
+      <div className="mt-[40px] flex items-center gap-1 text-md-semibold leading-[24px] md:mt-[174px] font-semibold leading-24">
         <Image src={miniStar} alt="Star" width={16} height={16} />
         <span>
           {rating} ({reviewCount})
         </span>
       </div>
-      <h1 className="mt-6 line-clamp-2 text-2xl-bold leading-[26px] md:mt-[20px] md:text-3xl-bold md:leading-[42px]">
+      <h2 className="mt-6 line-clamp-2 text-xl-bold leading-[26px] md:mt-[20px] md:text-2xl-bold md:leading-[42px]">
         {title}
-      </h1>
+      </h2>
       <div className="absolute bottom-[24px] md:bottom-[30px]">
         <span className="font-bold leading-[26px]">₩ {price.toLocaleString()} </span>
-        <span className="text-lg-medium leading-[24px] text-primary-gray-450">/ 인</span>
+        <span className="text-lg-medium leading-[24px] text-primary-gray-450">/ 원</span>
       </div>
     </Link>
   );
