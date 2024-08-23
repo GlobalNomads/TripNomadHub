daf9265fb651fc0533fc00830749cfb4ffa77f71
YoungUnKim <162089313+YoungUnKim@users.noreply.github.com>
2024-08-23 14:14:09 +0900

    fix: 새로고침 대응..

--- app/hooks/useCleanupOnUnload.ts
+++ app/hooks/useCleanupOnUnload.ts
@@ -4,28 +4,30 @@ import { useEffect } from "react";
 
 const useCleanupOnUnload = () => {
   useEffect(() => {
+    if (typeof window === "undefined") return; // 서버 사이드 렌더링에서는 아무 것도 하지 않음
+
     const deleteCookie = (name: string) => {
       document.cookie = `${name}=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/`;
     };
 
-    const handleBeforeUnload = (event: BeforeUnloadEvent) => {
-      // 페이지가 종료되거나 새로고침 될 때의 로직
-      if (sessionStorage.getItem("isReload") === "true") {
-        // 새로고침의 경우, sessionStorage에 플래그를 설정하고 쿠키를 삭제하지 않음
-        sessionStorage.removeItem("isReload");
-      } else {
-        // 페이지 종료 시 쿠키 삭제
+    const handleVisibilityChange = () => {
+      // 페이지가 숨겨질 때 새로고침 플래그 설정
+      if (document.visibilityState === "hidden") {
+        sessionStorage.setItem("isReload", "true");
+      }
+    };
+
+    const handleUnload = () => {
+      // 페이지 종료 시 쿠키 삭제
+      if (sessionStorage.getItem("isReload") !== "true") {
         deleteCookie("accessToken");
         deleteCookie("refreshToken");
       }
-
-      // 페이지를 떠나기 전에 경고 메시지를 표시할 수 있습니다.
-      event.preventDefault();
-      event.returnValue = ""; // 일부 브라우저에서 경고 메시지를 표시하기 위해 필요
+      sessionStorage.removeItem("isReload"); // 종료 후 플래그 제거
     };
 
     const handleLoad = () => {
-      // 페이지 로드 시, 새로고침인지 확인하기 위한 플래그 설정
+      // 페이지가 로드될 때 새로고침 여부를 확인하고 플래그를 제거
       if (performance.navigation.type === performance.navigation.TYPE_RELOAD) {
         sessionStorage.setItem("isReload", "true");
       } else {
@@ -33,12 +35,19 @@ const useCleanupOnUnload = () => {
       }
     };
 
-    window.addEventListener("beforeunload", handleBeforeUnload);
+    // 페이지 로드 시 플래그 초기화
     window.addEventListener("load", handleLoad);
 
+    // 페이지가 숨겨질 때 플래그 설정
+    document.addEventListener("visibilitychange", handleVisibilityChange);
+
+    // 페이지 종료 시 쿠키 삭제
+    window.addEventListener("unload", handleUnload);
+
     return () => {
-      window.removeEventListener("beforeunload", handleBeforeUnload);
       window.removeEventListener("load", handleLoad);
+      document.removeEventListener("visibilitychange", handleVisibilityChange);
+      window.removeEventListener("unload", handleUnload);
     };
   }, []);
 };
