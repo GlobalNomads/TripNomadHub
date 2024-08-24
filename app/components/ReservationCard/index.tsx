ebff0cb535b32f713dc166e21868f15bcbf2e42c
jinyun <truejinyun@gmail.com>
2024-08-25 07:23:59 +0900

    feat: css조정

--- app/components/ReservationCard/index.tsx
+++ app/components/ReservationCard/index.tsx
@@ -1,11 +1,11 @@
-import star from "@icon/ic_star_on.svg";
+import miniStar from "@icon/mini_star.svg";
 import Image from "next/image";
 import React from "react";
 
 // <1> 이미지 컴포넌트
 const ImageComponent = <T,>({ getImageUrl, item }: { getImageUrl: (item: T) => string; item: T }) => (
   <div className="relative h-[128px] w-[128px] flex-shrink-0 overflow-hidden rounded-l-3xl md:h-[156px] md:w-[156px] xl:h-[204px] xl:w-[204px]">
-    <Image src={getImageUrl(item)} priority layout="fill" objectFit="cover" alt="" />
+    <Image src={getImageUrl(item)} priority layout="fill" objectFit="cover" alt="체험 배너 사진" />
   </div>
 );
 
@@ -21,14 +21,14 @@ const RatingComponent = <T,>({
   getCategory?: (item: T) => string;
   item: T;
 }) => (
-  <div className="text-sm font-bold md:text-lg">
+  <div className="text-sm-semibold text-primary-black-200 md:text-md-semibold xl:text-lg-semibold">
     {getCategory ? (
       <span>{getCategory(item)}</span>
     ) : getRating && getReviewCount ? (
       <span className="flex items-center space-x-[4px] md:space-x-[6px]">
-        <Image src={star} alt="rating" width={16} height={16} />
-        <span>{getRating(item)}</span>
-        <span>({getReviewCount(item)})</span>
+        <Image src={miniStar} alt="rating" width={19} height={19} />
+        <span className="font-bold">{getRating(item)}</span>
+        <span className="text-primary-gray-800">({getReviewCount(item)})</span>
       </span>
     ) : null}
   </div>
@@ -46,6 +46,8 @@ const SubtitleComponent = <T,>({ getSubtitle, item }: { getSubtitle?: (item: T) 
   </div>
 );
 
+import PriceComponent from "./PriceComponent";
+
 // <5> 가격 및 자식 컴포넌트
 const PriceAndChildrenComponent = <T,>({
   getPrice,
@@ -56,16 +58,18 @@ const PriceAndChildrenComponent = <T,>({
   children?: React.ReactNode;
   item: T;
 }) => {
-  const formattedPrice = getPrice
-    ? new Intl.NumberFormat("en-US", {
-        style: "currency",
-        currency: "KRW",
-      }).format(getPrice(item)) + "/인"
-    : "";
-
   return (
     <div className="flex w-full items-center justify-between">
-      <div className="md:text-sm-regular text-xs-regular xl:text-[18px]">{formattedPrice}</div>
+      <PriceComponent
+        getPrice={getPrice}
+        item={item}
+        render={formattedPrice => (
+          <div className="flex items-center text-lg-semibold text-primary-gray-800 md:text-xl-semibold xl:text-2xl-semibold">
+            <span>{formattedPrice}</span>
+            <span className="md:text-lg-16px-medium ml-1 text-md-regular text-gray-500 xl:text-lg-medium">/인</span>
+          </div>
+        )}
+      />
       {children}
     </div>
   );
@@ -95,11 +99,11 @@ const ReservationCard = <T,>({
   children,
 }: ReservationCardProps<T>) => {
   return (
-    <div className="mx-auto flex flex-col gap-2 md:gap-4 xl:gap-6">
+    <div className="mx-auto flex flex-col gap-2 md:w-[429px] md:gap-4 xl:w-[792px] xl:gap-6">
       {reservations.map((reservation, index) => (
         <div
           key={index}
-          className="flex h-[128px] rounded-3xl border border-solid border-gray-300 bg-white md:h-[156px] md:w-[429px] xl:h-[204px] xl:w-[792px]"
+          className="flex h-[128px] rounded-3xl border border-solid border-gray-300 bg-white md:h-[156px] xl:h-[204px]"
         >
           {/* <1> 이미지 컴포넌트 */}
           <ImageComponent getImageUrl={getImageUrl} item={reservation} />
